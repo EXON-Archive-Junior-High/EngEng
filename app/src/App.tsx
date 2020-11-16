55e7a7be6ae5ee15b7cd3ad69aef9a6db57f8788
시혁 박 <exon.indiv@gmail.com>
2020-11-16 22:01:39 +0900

    input

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './css/App.css'
+import Input from './components/Input'
 
 class App extends Component {
     public render() {
         return (
             <div className="App">
-                
+                <Input/>
             </div>
         )
     }
---
+++ app/src/components/Input.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+
+class Input extends Component {
+    public render() {
+        return (
+            <div>
+                <form>
+                    <input></input>
+                    <button>버튼</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Input
