9a59c866da491edde9ca246102e127ce610bd1f6
시혁 박 <exon.indiv@gmail.com>
2020-11-18 21:51:22 +0900

    show result

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -7,13 +7,22 @@ type quizType = {
 }
 
 class Input extends Component<quizType> {
+
+    public showResult(isCorrect: boolean) {
+        const inputText = document.querySelector('#input') as HTMLInputElement
+        const result = document.querySelector('#result')
+        if (result) result.innerHTML = isCorrect ? '맞았습니다' : '틀렸습니다'
+        result?.classList.remove(isCorrect ? 'fail' : 'correct')
+        result?.classList.add(isCorrect ? 'correct' : 'fail')
+        inputText.value = ''
+    }
+
     public click() {
         const inputText = document.querySelector('#input') as HTMLInputElement
         if (inputText.value.toLowerCase() === this.props.answer) {
-            alert('맞았습니다')
-        } else {
-            inputText.value = ''
+            this.showResult(true)
         }
+        else this.showResult(false)
     }
 
     public handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -27,6 +36,8 @@ class Input extends Component<quizType> {
                     <input id="input"></input>
                     <button onClick={this.click.bind(this)}>제출</button>
                 </form>
+                <br />
+                <p id="result" className="correct"></p>
             </div>
         )
     }
